be7119cc4b1e40bae8172f8fc1c9d0a3689eb14c
Eren <erendenek00@gmail.com>
2020-03-17 20:12:58 +0300

    Create alg-1.cpp

---
+++ C++Algorithms/alg-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string isim,soyad;
+int yas;
+
+
+int main()
+{
+	cout << "Lutfen isminizi giriniz: ";
+	cin >> isim;
+   
+  cout << "Lutfen soyadinizi giriniz: ";
+	cin >> soyad;
+		
+	cout << "Lutfen yasinizi giriniz: ";
+	cin >> yas;
+    
+  cout << "Ismiziniz: " << isim << " Soyadiniz: " << soyad << " Yasiniz: " << yas;
+	 
+	return 0;
+}
