1aeb19e0287b31bf4c4ec9322d5582b04a44b660
Eren <erendenek00@gmail.com>
2020-03-18 16:11:01 +0300

    Create Alg-203.cpp

---
+++ C++Algorithms/Alg-203.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>   
+
+using namespace std;
+
+#define element 10
+
+int randomArr[element];
+int reverseArr[element];
+
+int minVal,maxVal;
+
+int main()
+{
+	
+	for(int i=0;i<element;i++)
+	{
+		randomArr[i] = rand();
+		cout << randomArr[i] << endl;
+	}
+	
+	cout << "************" << endl;
+	
+	for(int i=0;i<element;i++)
+	{
+		reverseArr[i] = randomArr[element-i-1];
+		cout << reverseArr[i] << endl;
+	}
+	
+	
+	minVal = reverseArr[0];
+	maxVal = reverseArr[0];
+	
+	for(int i=0;i<element;i++)
+	{
+		if( reverseArr[i] < minVal) minVal = reverseArr[i];
+		if( reverseArr[i] > maxVal) maxVal = reverseArr[i];
+	}
+	
+	cout << "En kucuk deger : " << minVal << endl;
+	cout << "En buyuk deger : " << maxVal << endl;
+	return 0;
+}
